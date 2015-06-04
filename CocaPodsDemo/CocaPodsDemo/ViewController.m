e214a28bc997a0e781a8dc1922c3857de3688dc0
wgl7569 <wgl7569@wgl7569deMac.local>
2015-06-04 14:29:20 +0800

    2

--- CocaPodsDemo/CocaPodsDemo/ViewController.m
+++ CocaPodsDemo/CocaPodsDemo/ViewController.m
@@ -46,28 +46,69 @@
 //        make.edges.equalTo(sv).width.insets(UIEdgeInsetsMake(10, 10, 10, 10));
 //    }];
     
-    UIView *sv2 = [UIView new];
-    UIView *sv3 = [UIView new];
-
-    sv2.backgroundColor = [UIColor redColor];
-    sv3.backgroundColor = [UIColor redColor];
-
-    [sv addSubview:sv2];
-    [sv addSubview:sv3];
-    [sv2 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerY.mas_equalTo(sv.mas_centerY);
-        make.left.equalTo(sv.mas_left).with.offset(10);
-        make.right.equalTo(sv3.mas_left).with.offset(-10);
-        make.height.mas_equalTo(@150);
-        make.width.equalTo(sv3);
+//    UIView *sv2 = [UIView new];
+//    UIView *sv3 = [UIView new];
+//
+//    sv2.backgroundColor = [UIColor redColor];
+//    sv3.backgroundColor = [UIColor redColor];
+//
+//    [sv addSubview:sv2];
+//    [sv addSubview:sv3];
+//    [sv2 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.centerY.mas_equalTo(sv.mas_centerY);
+//        make.left.equalTo(sv.mas_left).with.offset(10);
+//        make.right.equalTo(sv3.mas_left).with.offset(-10);
+//        make.height.mas_equalTo(@150);
+//        make.width.equalTo(sv3);
+//    }];
+//    [sv3 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.centerY.equalTo(sv.mas_centerY);
+//        make.left.equalTo(sv2.mas_right).with.offset(10);
+//        make.right.equalTo(sv.mas_right).with.offset(-10);
+//        make.height.mas_equalTo(@150);
+//        make.width.mas_equalTo(sv2);
+//    }];
+    
+    UIScrollView *scrollView  = [UIScrollView new];
+    scrollView.backgroundColor  = [UIColor whiteColor];
+    [sv addSubview:scrollView];
+    [scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(sv).with.insets(UIEdgeInsetsMake(5, 6, 6, 6));
     }];
-    [sv3 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerY.equalTo(sv.mas_centerY);
-        make.left.equalTo(sv2.mas_right).with.offset(10);
-        make.right.equalTo(sv.mas_right).with.offset(-10);
-        make.height.mas_equalTo(@150);
-        make.width.mas_equalTo(sv2);
+    UIView *container = [UIView new];
+//    container.backgroundColor = [UIColor greenColor];
+    [scrollView addSubview:container];
+    [container mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(scrollView);
+        make.width.equalTo(scrollView);
     }];
+    int count = 10;
+    UIView *listView  = nil;
+    for (int i = 0; i < count ;i++){
+        UIView *subView = [UIView new];
+        [container addSubview:subView];
+        subView.backgroundColor = [UIColor colorWithHue:( arc4random() % 256 / 256.0 )
+                                          saturation:( arc4random() % 128 / 256.0 ) + 0.5
+                                          brightness:( arc4random() % 128 / 256.0 ) + 0.5
+                                               alpha:1];
+        [subView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.and.right.equalTo(container);
+            make.height.equalTo(@(20*i));
+            if (listView){
+                make.top.equalTo(listView.mas_bottom);
+            }
+            else{
+                make.top.equalTo(container.mas_top);
+            }
+
+        }];
+        listView = subView;
+        
+    }
+    [container mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(listView.mas_bottom);
+    }];
+    
 //    [sv2 mas_makeConstraints:^(MASConstraintMaker *make) {
 //        make.centerY.mas_equalTo(sv.mas_centerY);
 //        make.left.equalTo(sv.mas_left).with.offset(padding1);
