6dac408d3662c6fdb6ca29b09f8779ac857f4f74
Wang Guolei <wangguolei@leqian.com>
2015-08-26 11:30:03 +0800

    add md

---
+++ TestDemo/信条纸/信条纸/CustomStatusBar.h
@@ -0,0 +1,13 @@
+//
+//  CustomStatusBar.h
+//  信条纸
+//
+//  Created by lq on 15/8/18.
+//  Copyright (c) 2015年 wgl7569. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomStatusBar : UIWindow
+
+@end
---
+++ TestDemo/信条纸/信条纸/CustomStatusBar.m
@@ -0,0 +1,21 @@
+//
+//  CustomStatusBar.m
+//  信条纸
+//
+//  Created by lq on 15/8/18.
+//  Copyright (c) 2015年 wgl7569. All rights reserved.
+//
+
+#import "CustomStatusBar.h"
+
+@implementation CustomStatusBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
